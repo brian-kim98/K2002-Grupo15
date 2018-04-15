e3234c3b84dc47d79a481f62bfc349771b9d7148
Lautaro Olivera <olivera4lautaro@gmail.com>
2018-04-15 14:45:49 -0300

    Nuevos eventos: cambios en el tipo y tests
    luchotocayseva y luchoes ahorrante pasan a ser de tipo transaccion, y modificamos los tests correspondientes

--- Entrega TP 1 - Primera Parte.hs
+++ Entrega TP 1 - Primera Parte.hs
@@ -14,7 +14,7 @@ deposito cantidadADepositar = (+ cantidadADepositar)
 
 extraccion :: Float -> Evento
 extraccion cantidadAExtraer carteraOnline | carteraOnline >= cantidadAExtraer = carteraOnline - cantidadAExtraer
-                             | otherwise = 0
+                                          | otherwise = 0
 
 upgrade :: Evento
 -- Creamos una funcion llamada "Bonus" --
@@ -22,7 +22,7 @@ bonus = (* 0.2)
 
 
 upgrade carteraOnline | ((<= 10). bonus) carteraOnline = carteraOnline + bonus carteraOnline
-                  | otherwise = carteraOnline + 10
+                      | otherwise = carteraOnline + 10
 
 cierreDeCuenta :: Evento
 cierreDeCuenta = (*0)
@@ -79,14 +79,14 @@ ahorranteErrante :: Evento
 ahorranteErrante = ((deposito 10) . upgrade . (deposito 8) . (extraccion 1) . (deposito 2) . (deposito 1))
 
 
-luchoTocaYSeVa :: Usuario -> Float
-luchoTocaYSeVa unUsuario |nombre unUsuario == "Luciano" = tocoYMeVoy (billetera unUsuario)
-                         |otherwise = quedaIgual (billetera unUsuario)
+luchoTocaYSeVa :: Transaccion
+luchoTocaYSeVa unUsuario |nombre unUsuario == "Luciano" = tocoYMeVoy
+                         |otherwise = quedaIgual
 
 
-luchoEsAhorranteErrante :: Usuario -> Float
-luchoEsAhorranteErrante unUsuario |nombre unUsuario == "Luciano" = ahorranteErrante (billetera unUsuario)
-                                  |otherwise = quedaIgual (billetera unUsuario)
+luchoEsAhorranteErrante :: Transaccion
+luchoEsAhorranteErrante unUsuario |nombre unUsuario == "Luciano" = ahorranteErrante
+                                  |otherwise = quedaIgual
 
 
 pepeLeDa7UnidadesALucho :: Transaccion
@@ -98,24 +98,24 @@ pepeLeDa7UnidadesALucho unUsuario | nombre unUsuario == "Jose" = extraccion 7
 -- Vamos con los Tests ahora :D --
 ejecutarTests = hspec $ do
  describe "Operaciones de Eventos " $ do
-   it "Depositar 10 en una billetera de 10 monedas = 20" $ deposito 10 10 `shouldBe` 20
-   it "Extraer 3 de una billetera de 10 monedas = 7" $ extraccion 3 10 `shouldBe` 7
-   it "Extraer 15 de una billetera de 10 monedas = 0" $ extraccion 15 10 `shouldBe` 0
-   it "Upgrade a una billetera de 10 monedas = 12" $ upgrade 10 `shouldBe` 12
-   it "Cerrar la cuenta a una billetera de 10 monedas = 0" $ cierreDeCuenta 10 `shouldBe` 0
-   it "Que quede igual la billetera de 10 monedas = 10" $ quedaIgual 10 `shouldBe` 10
-   it "Depositar 1000 y realizar un upgrade a una billetera de 10 monedas = 1020" $ (upgrade . (deposito 1000)) 10 `shouldBe` 1020
+   it "Depositar 10 en una billetera de 10 monedas = 20 monedas" $ deposito 10 10 `shouldBe` 20
+   it "Extraer 3 de una billetera de 10 monedas = 7 monedas" $ extraccion 3 10 `shouldBe` 7
+   it "Extraer 15 de una billetera de 10 monedas = 0 monedas" $ extraccion 15 10 `shouldBe` 0
+   it "Upgrade a una billetera de 10 monedas = 12 monedas" $ upgrade 10 `shouldBe` 12
+   it "Cerrar la cuenta a una billetera de 10 monedas = 0 monedas" $ cierreDeCuenta 10 `shouldBe` 0
+   it "Que quede igual la billetera de 10 monedas = 10 monedas" $ quedaIgual 10 `shouldBe` 10
+   it "Depositar 1000 y realizar un upgrade a una billetera de 10 monedas = 1020 monedas" $ (upgrade . (deposito 1000)) 10 `shouldBe` 1020
  describe "Operaciones de Usuarios " $ do
    it "La billetera de Pepe debería ser de 10 monedas" $ billetera pepe `shouldBe` 10
    it "El cierre de cuenta de la billetera de Pepe quedaría en 0 monedas" $ cierreDeCuenta (billetera pepe) `shouldBe` 0
    it "La billetera de Pepe, luego del deposito de 15 monedas, extraer 2 y tener un upgrade, tiene 27,6 monedas" $ (upgrade . (extraccion 2) . (deposito 15)) (billetera pepe) `shouldBe` 27.6
  describe "Operaciones de Transacciones " $ do
-   it "Aplicamos la transaccion lucho cierra su cuenta, a pepe y la funcion que nos da lo aplicamos a una billetera de 20 monedas, esto da 20 ya que queda igual " $ (luchoCierraLaCuenta pepe) 20 `shouldBe` 20
-   it "La transacción 'pepe deposita 5 monedas' se aplica a Pepe. El resultad lo aplicamos a una billetera de 10 monedas, deberia quedar con 15" $ (pepeDeposita5Monedas pepe) 10 `shouldBe` 15
-   it "La transaccion 'pepe deposita 5 monedas' se aplica al Pepe2. El evento resultante se aplica a una billetera de 50 monedas, y debería quedar con 55." $ (pepeDeposita5Monedas pepe2) 50 `shouldBe` 55
+   it "Aplicamos la transaccion 'lucho cierra su cuenta' con pepe, que cuenta con una billetera de 20 monedas = 20 monedas" $ (luchoCierraLaCuenta pepe) 20 `shouldBe` 20
+   it "Aplicamos la transacción 'pepe deposita 5 monedas' con pepe, que cuenta con una billetera de 10 monedas = 15 monedas" $ (pepeDeposita5Monedas pepe) 10 `shouldBe` 15
+   it "Aplicamos la transaccion 'pepe deposita 5 monedas' con pepe2, que cuenta con una billetera de 50 monedas = 55 monedas" $ (pepeDeposita5Monedas pepe2) 50 `shouldBe` 55
  describe "Operaciones de Nuevos Eventos " $ do
-   it "Un usuario con 10 monedas toca y se va, terminaria con 0 monedas" $ (tocoYMeVoy 10) `shouldBe` 0
-   it "Un usuario con 10 monedas es ahorrante errante, deberia terminar con 34 monedas " $ (ahorranteErrante 10) `shouldBe` 34
+   it "Si Lucho 'Toca y Se Va', aplicado a una billetera de 10 monedas = 0 " $ (luchoTocaYSeVa lucho) 10 `shouldBe` 0
+   it "Si Lucho es un 'Ahorrante Errante', aplicado a una billetera de 10 monedas = 34 monedas" $ (luchoEsAhorranteErrante lucho) 10 `shouldBe` 34
  describe "Transacciones mas complejas " $ do
    it "Aplicamos la transaccion 'pepe le da 7 unidades a lucho' con Pepe, que cuenta con una billetera de 10 monedas, y termina con 3 monedas " $ (pepeLeDa7UnidadesALucho pepe 10)`shouldBe` 3
    it "Aplicamos la transaccion 'pepe le da 7 unidades a lucho' con Lucho, que cuenta con una billetera de 10 monedas, termina con 17 monedas " $ (pepeLeDa7UnidadesALucho lucho 10) `shouldBe` 17
