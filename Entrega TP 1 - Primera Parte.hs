3d181aa51025e56cdb83582fa5443479252099e1
Leon Reynosa <leonreynosa98@gmail.com>
2018-04-23 16:18:17 -0300

    Correcion de 1era entrega
    -Arreglo de depòsito con funciòn max
    -Mejora de extracciòn con funciòn max
    -Mejora de upgrade con funciòn min
    -Mejora de funciòn "quedaIgual" con id
    -Sacamos guardas sobrantes
    -Definiciòn del tipo Billetera 
    -Reubicaciòn de tests para mejor lectura
    -Creaciòn de generadores de transacciones y transferencias de dinero

--- Entrega TP 1 - Primera Parte.hs
+++ Entrega TP 1 - Primera Parte.hs
@@ -4,40 +4,59 @@ import Data.List
 import Data.Maybe
 import Test.Hspec
 
+type Dinero = Float
 
--- Definimos el tipo Evento --
-type Evento = Float -> Float
+type Billetera = Float
 
-deposito ::Float -> Evento
-deposito cantidadADepositar = (+ cantidadADepositar)
+type Evento = Billetera -> Billetera
+
+type Nombre = String
+
+type Transaccion = Usuario -> Evento -> Usuario -> Evento
+
+type Transferencias = Usuario -> Usuario -> Usuario -> Dinero -> Evento
+
+data Usuario = Usuario {
+nombre :: Nombre,
+billetera :: Dinero
+} deriving (Show, Eq)
 
 
-extraccion :: Float -> Evento
-extraccion cantidadAExtraer carteraOnline | carteraOnline >= cantidadAExtraer = carteraOnline - cantidadAExtraer
-                                          | otherwise = 0
+deposito :: Dinero -> Evento
+deposito cantidadADepositar carteraOnline = (+ carteraOnline) (max 0.0 cantidadADepositar)
+
+
+extraccion :: Dinero -> Evento
+extraccion cantidadAExtraer carteraOnline | (max 0 cantidadAExtraer) <= carteraOnline = carteraOnline - (max 0 cantidadAExtraer)
+
 
 upgrade :: Evento
--- Creamos una funcion llamada "Bonus" --
 bonus = (* 0.2)
-
-
-upgrade carteraOnline | ((<= 10). bonus) carteraOnline = carteraOnline + bonus carteraOnline
-                      | otherwise = carteraOnline + 10
+upgrade carteraOnline = min 10 (bonus carteraOnline) + carteraOnline
 
 cierreDeCuenta :: Evento
 cierreDeCuenta = (*0)
 
 quedaIgual :: Evento
-quedaIgual = (* 1)
+quedaIgual = id
+
+tocoYMeVoy :: Evento
+tocoYMeVoy = (cierreDeCuenta . upgrade . (deposito 15))
+
+ahorranteErrante :: Evento
+ahorranteErrante = ((deposito 10) . upgrade . (deposito 8) . (extraccion 1) . (deposito 2) . (deposito 1))
+
+testEventos = hspec $ do
+ describe "Operaciones de Eventos " $ do
+   it "Depositar 10 en una billetera de 10 monedas = 20 monedas" $ deposito 10 10 `shouldBe` 20
+   it "Extraer 3 de una billetera de 10 monedas = 7 monedas" $ extraccion 3 10 `shouldBe` 7
+   it "Extraer 15 de una billetera de 10 monedas = 0 monedas" $ extraccion 15 10 `shouldBe` 0
+   it "Upgrade a una billetera de 10 monedas = 12 monedas" $ upgrade 10 `shouldBe` 12
+   it "Cerrar la cuenta a una billetera de 10 monedas = 0 monedas" $ cierreDeCuenta 10 `shouldBe` 0
+   it "Que quede igual la billetera de 10 monedas = 10 monedas" $ quedaIgual 10 `shouldBe` 10
+   it "Depositar 1000 y realizar un upgrade a una billetera de 10 monedas = 1020 monedas" $ (upgrade . (deposito 1000)) 10 `shouldBe` 1020
 
 
--- Definimos la data de un usuario --
-data Usuario = Usuario {
-nombre :: String,
-billetera :: Float
-} deriving (Show, Eq)
-
--- Creamos el pepe y el lucho que nos piden --
 pepe = Usuario {
 nombre = "Jose",
 billetera = 10
@@ -49,19 +68,16 @@ billetera = 2
 }
 
 
--- TRANSACCIONES --
+testUsuarios = hspec $ do
+ describe "Operaciones de Usuarios " $ do
+   it "La billetera de Pepe debería ser de 10 monedas" $ billetera pepe `shouldBe` 10
+   it "El cierre de cuenta de la billetera de Pepe quedaría en 0 monedas" $ cierreDeCuenta (billetera pepe) `shouldBe` 0
+   it "La billetera de Pepe, luego del deposito de 15 monedas, extraer 2 y tener un upgrade, tiene 27,6 monedas" $ (upgrade . (extraccion 2) . (deposito 15)) (billetera pepe) `shouldBe` 27.6
 
--- Definimos el tipo Transaccion --
-type Transaccion = Usuario -> Evento
 
-luchoCierraLaCuenta :: Transaccion
-luchoCierraLaCuenta unUsuario | nombre unUsuario == "Luciano" = cierreDeCuenta
-                              | otherwise = quedaIgual
-
-pepeDeposita5Monedas :: Transaccion
-pepeDeposita5Monedas unUsuario | nombre unUsuario == "Jose" = deposito 5
-                               | otherwise = quedaIgual
-
+generadorTransacciones :: Transaccion
+generadorTransacciones unaPersona unEvento otraPersona | nombre unaPersona == nombre otraPersona = unEvento
+                                                       | otherwise = quedaIgual
 
 -- Creamos el pepe 2 --
 pepe2 = Usuario {
@@ -69,53 +85,24 @@ nombre = "Jose",
 billetera = 20
 }
 
--- NUEVOS EVENTOS --
 
-tocoYMeVoy :: Evento
-tocoYMeVoy = (cierreDeCuenta . upgrade . (deposito 15))
-
-
-ahorranteErrante :: Evento
-ahorranteErrante = ((deposito 10) . upgrade . (deposito 8) . (extraccion 1) . (deposito 2) . (deposito 1))
-
-
-luchoTocaYSeVa :: Transaccion
-luchoTocaYSeVa unUsuario |nombre unUsuario == "Luciano" = tocoYMeVoy
-                         |otherwise = quedaIgual
+testTransacciones = hspec $ do
+  describe "Operaciones de Transacciones " $ do
+  it "Aplicamos la transaccion 'lucho cierra su cuenta' con pepe, y esto lo aplicamos a una billetera de 20 monedas = 20 monedas" $ (generadorTransacciones lucho cierreDeCuenta pepe 20) `shouldBe` 20
+  it "Aplicamos la transacción 'pepe deposita 5 monedas' con pepe, y esto lo aplicamos a con una billetera de 10 monedas = 15 monedas" $ (generadorTransacciones pepe (deposito 5) pepe 10) `shouldBe` 15
+  it "Aplicamos la transaccion 'pepe deposita 5 monedas' con pepe2, y esto lo aplicamos a una billetera de 50 monedas = 55 monedas" $ (generadorTransacciones pepe (deposito 5) pepe2 50)`shouldBe` 55
+  it "Aplicamos la transaccion 'lucho toca y se va' al mismo usuario, y esto lo aplicamos a una billetera de 10 monedas = 0" $ (generadorTransacciones lucho tocoYMeVoy lucho 10) `shouldBe` 0
+  it "Aplicamos la transaccion 'lucho ahorrante errante' al mismo usuario, y esto lo aplicamos a una billetera de 10 monedas = 34" $ (generadorTransacciones lucho ahorranteErrante lucho 10) `shouldBe` 34
 
 
-luchoEsAhorranteErrante :: Transaccion
-luchoEsAhorranteErrante unUsuario |nombre unUsuario == "Luciano" = ahorranteErrante
-                                  |otherwise = quedaIgual
+generadorTransferencias :: Transferencias
+generadorTransferencias usuarioDeudor usuarioAcreedor usuarioAplicado unaCantidad  | unaCantidad > (billetera usuarioDeudor) = error "El usuario deudor no tiene suficientes monedas para pagar"
+                                                                                   | nombre usuarioDeudor == nombre usuarioAplicado = (extraccion unaCantidad)
+                                                                                   | nombre usuarioAcreedor == nombre usuarioAplicado = (deposito unaCantidad)
+                                                                                   | otherwise = quedaIgual
 
 
-pepeLeDa7UnidadesALucho :: Transaccion
-pepeLeDa7UnidadesALucho unUsuario | nombre unUsuario == "Jose" = extraccion 7
-                                  | nombre unUsuario == "Luciano" = deposito 7
-                                  | otherwise = quedaIgual
-
-
--- Vamos con los Tests ahora :D --
-ejecutarTests = hspec $ do
- describe "Operaciones de Eventos " $ do
-   it "Depositar 10 en una billetera de 10 monedas = 20 monedas" $ deposito 10 10 `shouldBe` 20
-   it "Extraer 3 de una billetera de 10 monedas = 7 monedas" $ extraccion 3 10 `shouldBe` 7
-   it "Extraer 15 de una billetera de 10 monedas = 0 monedas" $ extraccion 15 10 `shouldBe` 0
-   it "Upgrade a una billetera de 10 monedas = 12 monedas" $ upgrade 10 `shouldBe` 12
-   it "Cerrar la cuenta a una billetera de 10 monedas = 0 monedas" $ cierreDeCuenta 10 `shouldBe` 0
-   it "Que quede igual la billetera de 10 monedas = 10 monedas" $ quedaIgual 10 `shouldBe` 10
-   it "Depositar 1000 y realizar un upgrade a una billetera de 10 monedas = 1020 monedas" $ (upgrade . (deposito 1000)) 10 `shouldBe` 1020
- describe "Operaciones de Usuarios " $ do
-   it "La billetera de Pepe debería ser de 10 monedas" $ billetera pepe `shouldBe` 10
-   it "El cierre de cuenta de la billetera de Pepe quedaría en 0 monedas" $ cierreDeCuenta (billetera pepe) `shouldBe` 0
-   it "La billetera de Pepe, luego del deposito de 15 monedas, extraer 2 y tener un upgrade, tiene 27,6 monedas" $ (upgrade . (extraccion 2) . (deposito 15)) (billetera pepe) `shouldBe` 27.6
- describe "Operaciones de Transacciones " $ do
-   it "Aplicamos la transaccion 'lucho cierra su cuenta' con pepe, que cuenta con una billetera de 20 monedas = 20 monedas" $ (luchoCierraLaCuenta pepe) 20 `shouldBe` 20
-   it "Aplicamos la transacción 'pepe deposita 5 monedas' con pepe, que cuenta con una billetera de 10 monedas = 15 monedas" $ (pepeDeposita5Monedas pepe) 10 `shouldBe` 15
-   it "Aplicamos la transaccion 'pepe deposita 5 monedas' con pepe2, que cuenta con una billetera de 50 monedas = 55 monedas" $ (pepeDeposita5Monedas pepe2) 50 `shouldBe` 55
- describe "Operaciones de Nuevos Eventos " $ do
-   it "Si Lucho 'Toca y Se Va', aplicado a una billetera de 10 monedas = 0 " $ (luchoTocaYSeVa lucho) 10 `shouldBe` 0
-   it "Si Lucho es un 'Ahorrante Errante', aplicado a una billetera de 10 monedas = 34 monedas" $ (luchoEsAhorranteErrante lucho) 10 `shouldBe` 34
- describe "Transacciones mas complejas " $ do
-   it "Aplicamos la transaccion 'pepe le da 7 unidades a lucho' con Pepe, que cuenta con una billetera de 10 monedas, y termina con 3 monedas " $ (pepeLeDa7UnidadesALucho pepe 10)`shouldBe` 3
-   it "Aplicamos la transaccion 'pepe le da 7 unidades a lucho' con Lucho, que cuenta con una billetera de 10 monedas, termina con 17 monedas " $ (pepeLeDa7UnidadesALucho lucho 10) `shouldBe` 17
+testTransferencias = hspec $ do
+  describe "Transacciones mas complejas " $ do
+  it "Aplicamos la transaccion 'pepe le da 7 unidades a lucho' con Pepe, esto lo aplicamos a una billetera de 10 monedas, y termina con 3 monedas " $ (generadorTransferencias pepe lucho pepe 7 10)`shouldBe` 3
+  it "Aplicamos la transaccion 'pepe le da 7 unidades a lucho' con Lucho, esto lo aplicamos a una billetera de 10 monedas, termina con 17 monedas " $ (generadorTransferencias pepe lucho lucho 7 10) `shouldBe` 17
